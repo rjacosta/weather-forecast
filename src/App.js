2be2800a74f7b56cb4d555acb7c33d8f6fb214a8
Romel Acosta <rjacosta@ucsd.edu>
2019-11-14 13:33:54 -0800

    refactoring App file

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
 import React, { Component } from "react";
 import "./style.css";
 import WeatherTile from "./components/WeatherTile";
+import * as Constants from "./constants/index";
+import { Spinner } from "spin.js";
+
+var target = document.getElementById("root");
+var spinner = new Spinner(Constants.SPINNER_PARAMS).spin();
 
 var apiBaseURL = "http://api.openweathermap.org/data/2.5/forecast";
 var apiKey = "dad308e1e9508fb8704e0e28fd983111";
 var cityKey = "5345679"; // Key for El Dorado Hills
 
-var weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-var days = [0, 1, 2, 3, 4, 5, 6];
-
-var SUNNY = "Clear";
-var CLOUDY = "Cloud";
-var SNOWY = "Snow";
-var RAINY = "Rain";
-
-function getWeeklyDays() {
-  var currentDate = new Date();
-  if (currentDate.getDay() === 0) {
-    return days;
-  }
-  days = days
-    .slice(currentDate.getDay())
-    .concat(days.slice(0, currentDate.getDay()));
-  return days;
-}
-
-function createWeatherTiles(weatherData) {
-  if (weatherData === null) return null;
-  return weatherData.map(data => {
-    data.day = weekdays[data.day];
-    return (
-      <li key={data.day}>
-        <WeatherTile data={data} />
-      </li>
-    );
-  });
-}
-
-function kelvinToFahrenheit(k) {
-  return ((k - 273.15) * 9) / 5 + 32;
-}
+var weekDays = {
+  0: "Sun",
+  1: "Mon",
+  2: "Tue",
+  3: "Wed",
+  4: "Thu",
+  5: "Fri",
+  6: "Sat"
+};
 
 class App extends Component {
   constructor(props) {
@@ -47,12 +27,21 @@ class App extends Component {
     this.state = {
       weatherData: null
     };
+    this.getWeatherData = this.getWeatherData.bind(this);
   }
 
   componentDidMount() {
-    getWeeklyDays();
+    target.appendChild(spinner.el);
+    this.getWeatherData();
+    setInterval(this.getWeatherData, 10000);
+  }
+
+  getWeatherData() {
     fetch(apiBaseURL + "?id=" + cityKey + "&APPID=" + apiKey)
-      .then(response => response.json())
+      .then(response => {
+        spinner.stop();
+        return response.json();
+      })
       .then(weatherData => {
         var weatherPoints = weatherData.list;
         weatherPoints.forEach(point => {
@@ -60,34 +49,20 @@ class App extends Component {
           point.jsDay = new Date(point.dt * 1000).getDay();
         });
         var filteredWeatherData = [];
-        days.forEach(day => {
+        Object.keys(weekDays).forEach(day => {
           var filteredPoints = weatherPoints.filter(
-            point => point.jsDay === day
+            point => point.jsDay === parseInt(day)
           );
           if (filteredPoints.length === 0) return;
-          var maxTemp = Number.MIN_SAFE_INTEGER,
-            minTemp = Number.MAX_SAFE_INTEGER;
-          var weatherCounter = {
-            sunny: 0,
-            cloudy: 0,
-            snowy: 0,
-            rainy: 0
-          };
-          filteredPoints.forEach(point => {
-            if (point.weather[0].main === SUNNY) weatherCounter.sunny++;
-            if (point.weather[0].main === CLOUDY) weatherCounter.cloudy++;
-            if (point.weather[0].main === SNOWY) weatherCounter.snowy++;
-            if (point.weather[0].main === RAINY) weatherCounter.rainy++;
-            if (point.main.temp_max > maxTemp) maxTemp = point.main.temp_max;
-            if (point.main.temp_min < minTemp) minTemp = point.main.temp_min;
-          });
+
+          var aggregatedWeatherDataPoint = this.getAggregatedWeatherDataPoint(
+            filteredPoints
+          );
           filteredWeatherData.push({
-            day: day,
-            weather: Object.keys(weatherCounter).reduce((a, b) =>
-              weatherCounter[a] > weatherCounter[b] ? a : b
-            ),
-            maxTemp: Math.round(kelvinToFahrenheit(maxTemp)),
-            minTemp: Math.round(kelvinToFahrenheit(minTemp))
+            day: parseInt(day),
+            weather: aggregatedWeatherDataPoint.weatherType,
+            maxTemp: aggregatedWeatherDataPoint.maxTemp,
+            minTemp: aggregatedWeatherDataPoint.minTemp
           });
         });
         this.setState({
@@ -96,16 +71,78 @@ class App extends Component {
       });
   }
 
-  render() {
-    var weatherTiles = createWeatherTiles(this.state.weatherData);
-    return (
-        <div className="Background">
-          <div className="weather-forecast">
-          <ul className="list-style">{weatherTiles}</ul>
-          </div>
-          <footer></footer>
-        </div>
+  getAggregatedWeatherDataPoint(filteredPoints) {
+    var aggregatedWeatherDataPoint = {
+      maxTemp: Number.MIN_SAFE_INTEGER,
+      minTemp: Number.MAX_SAFE_INTEGER,
+      weatherType: ""
+    };
+    var weatherCounter = {
+      Clear: 0,
+      Cloud: 0,
+      Snow: 0,
+      Rain: 0
+    };
+    filteredPoints.forEach(point => {
+      var weather = point.weather[0].main;
+      if (weather === Constants.SUNNY) weatherCounter.Clear++;
+      if (weather === Constants.CLOUDY) weatherCounter.Cloud++;
+      if (weather === Constants.SNOWY) weatherCounter.Snow++;
+      if (weather === Constants.RAINY) weatherCounter.Rain++;
+      if (point.main.temp_max > aggregatedWeatherDataPoint.maxTemp)
+        aggregatedWeatherDataPoint.maxTemp = point.main.temp_max;
+      if (point.main.temp_min < aggregatedWeatherDataPoint.minTemp)
+        aggregatedWeatherDataPoint.minTemp = point.main.temp_min;
+    });
+    aggregatedWeatherDataPoint.weatherType = Object.keys(
+      weatherCounter
+    ).reduce((a, b) => (weatherCounter[a] > weatherCounter[b] ? a : b));
+    aggregatedWeatherDataPoint.maxTemp = Math.round(
+      this.kelvinToFahrenheit(aggregatedWeatherDataPoint.maxTemp)
     );
+    aggregatedWeatherDataPoint.minTemp = Math.round(
+      this.kelvinToFahrenheit(aggregatedWeatherDataPoint.minTemp)
+    );
+    return aggregatedWeatherDataPoint;
+  }
+
+  kelvinToFahrenheit(k) {
+    return ((k - 273.15) * 9) / 5 + 32;
+  }
+
+  render() {
+    var weatherTiles = this.createWeatherTiles(this.state.weatherData);
+    return (
+      <div className="Background">
+        <div className="weather-forecast">
+          <ul className="list-style">{weatherTiles}</ul>
+        </div>
+        <footer></footer>
+      </div>
+    );
+  }
+
+  createWeatherTiles(weatherData) {
+    if (weatherData === null) return null;
+    var currentDay = new Date().getDay();
+    if (currentDay !== 0) {
+      var currentDayIndex = this.state.weatherData.findIndex(
+        data => data.day === currentDay
+      );
+
+      weatherData = weatherData
+        .slice(currentDayIndex)
+        .concat(weatherData.slice(0, currentDayIndex));
+    }
+
+    return weatherData.map(data => {
+      data.day = weekDays[data.day];
+      return (
+        <li key={data.day}>
+          <WeatherTile data={data} />
+        </li>
+      );
+    });
   }
 }
 
--- src/components/WeatherTile.js
+++ src/components/WeatherTile.js
@@ -4,34 +4,37 @@ import RainyIcon from "../resources/rainy_icon.svg";
 import SnowyIcon from "../resources/snowy_icon.svg";
 import SunnyIcon from "../resources/sunny_icon.svg";
 
+import * as Constants from "../constants/index";
+
 import "../style.css";
-
-var CLOUDY = "cloudy";
-var RAINY = "rainy";
-var SNOWY = "snowy";
-var SUNNY = "sunny";
-
-function getWeatherIcon(weather) {
-  if (weather === CLOUDY) return CloudyIcon;
-  if (weather === RAINY) return RainyIcon;
-  if (weather === SNOWY) return SnowyIcon;
-  if (weather === SUNNY) return SunnyIcon;
-}
 
 class WeatherTile extends Component {
 
+  constructor(props) {
+    super(props)
+    this.getWeatherIcon = this.getWeatherIcon.bind(this);
+  }
+
   render() {
-    var weatherIcon = getWeatherIcon(this.props.data.weather);
+    var weatherIcon = this.getWeatherIcon(this.props.data.weather);
     return (
       <div>
         <header>{this.props.data.day}</header>
         <img className="weather-icon" src={weatherIcon} alt={this.props.data.day} />
         <div>
-          {this.props.data.maxTemp}{"\xB0"} {this.props.data.minTemp}{"\xB0"}
+          H: {this.props.data.maxTemp}{"\xB0"} L: {this.props.data.minTemp}{"\xB0"}
         </div>
       </div>
     );
   }
+
+  getWeatherIcon(weather) {
+    if (weather === Constants.CLOUDY) return CloudyIcon;
+    if (weather === Constants.RAINY) return RainyIcon;
+    if (weather === Constants.SNOWY) return SnowyIcon;
+    if (weather === Constants.SUNNY) return SunnyIcon;
+  }
+
 }
 
 export default WeatherTile;
